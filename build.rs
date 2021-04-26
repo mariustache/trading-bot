e7533960eca9e063faedd3be6f0ec4100555d87e
shardamaka <mariusalexandrutache@gmail.com>
2021-04-26 20:50:42 +0300

    Updated build script to support scalability.

--- build.rs
+++ build.rs
@@ -1,10 +1,34 @@
 extern crate protoc_rust;
+use std::fs;
+use std::path::Path;
+
+const PROTOS_OUT_DIR: &str = "src/protos";
+const PROTOS_INCLUDES: [&'static str; 1] = [
+    "protos/binance",
+];
+const PROTOS_INPUTS: [&'static str; 3] = [
+    "protos/binance/common.proto",
+    "protos/binance/market_data.proto",
+    "protos/binance/wallet.proto",
+];
 
 fn main() {
-    protoc_rust::Codegen::new()
-        .out_dir("src/protos")
-        .inputs(&["protos/binance/common.proto", "protos/binance/wallet.proto"])
-        .include("protos/binance")
-        .run()
-        .expect("protoc failed.");
+    // Create output folder for generated code.
+    if Path::new(PROTOS_OUT_DIR).is_dir() == false {
+        let res = fs::create_dir(PROTOS_OUT_DIR);
+        match res {
+            Ok(v) => println!("Created folder {:?}", v),
+            Err(e) => panic!("{:?}", e)
+        }
+    }
+    
+    let mut generator = protoc_rust::Codegen::new();
+    generator.out_dir(PROTOS_OUT_DIR);
+    generator.inputs(&PROTOS_INPUTS);
+
+    for _include in &PROTOS_INCLUDES {
+        generator.include(_include);
+    }
+    
+    generator.run().expect("protoc failed.");
 }
